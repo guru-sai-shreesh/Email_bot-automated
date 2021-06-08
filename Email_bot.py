de66d1154b69878488f3bd5e2aac62e7504bb17b
guru-sai-shreesh <guru.sai.shreesh@gmail.com>
2021-06-08 21:28:24 +0530

    Update Email_bot.py

--- Email_bot.py
+++ Email_bot.py
@@ -4,11 +4,11 @@ import pyttsx3
 from email.message import EmailMessage
 import openpyxl as xl
 
-wb=xl.load_workbook('contacts.xlsx')
+wb = xl.load_workbook('contacts.xlsx')
 sheet = wb['Sheet1']
 contact_list = {}
 x = 2
-for x in range(2, sheet.max_row+1):
+for x in range(2, sheet.max_row + 1):
     cell1 = sheet.cell(x, 2)
     cell2 = sheet.cell(x, 3)
     contact_list[cell1.value] = cell2.value
@@ -16,10 +16,8 @@ for x in range(2, sheet.max_row+1):
 listener = sr.Recognizer()
 
 engine = pyttsx3.init()
-engine.setProperty('rate', 180) #reduces WPM to 180
-engine.setProperty('voice', 'com.apple.speech.synthesis.voice.samantha.premium') #This voice is best suited in macos
-
-
+engine.setProperty('rate', 180)  # reduces WPM to 180
+engine.setProperty('voice', 'com.apple.speech.synthesis.voice.samantha.premium')  # This voice is best suited in macos
 
 
 def talk(text):
@@ -40,8 +38,8 @@ def mike_out():
 
 
 def send_email(receiver, subject, message):
-    server = smtplib.SMTP('smtp.gmail.com', 587) #to connect to server
-    server.starttls() #tells that it is an secure connection
+    server = smtplib.SMTP('smtp.gmail.com', 587)  # to connect to server
+    server.starttls()  # tells that it is an secure connection
     # Make sure to give app access in your Google account
     server.login('gojo.testing123@gmail.com', 'hellogojo')
     email = EmailMessage()
@@ -63,14 +61,14 @@ def create_email():
     for name in names:
         if name not in contact_list:
             names.remove(name)
-    receivers=[]
-    if len(names)==0:
+    receivers = []
+    if len(names) == 0:
         talk("sorry there are no similar email addresses in your contacts")
         talk("please try again later")
         exit()
     for name in names:
         receivers.append(contact_list[name])
-    print("Receiver's Email adresses: ", *receivers)
+    print("Receiver's Email addresses: ", *receivers)
     print('Subject of this Email: ', end=' ')
     talk('What is the subject of this Email?')
     subject = mike_out()
@@ -88,11 +86,11 @@ def create_email():
 
     talk('Do you want to send another email?')
     send_more = mike_out()
-    if send_more=='yes' or send_more=='yep':
-        isnew_ornot()
+    if send_more == 'yes' or send_more == 'yep':
+        new_or_not()
 
 
-def isnew_ornot():
+def new_or_not():
     print("Say 'YES' if it is known contact else say 'NO': ")
     talk('Are you sending this email to a know contact?')
     opt = mike_out()
@@ -103,10 +101,10 @@ def isnew_ornot():
         name = input("Type name of new contact to add: ")
         email = input("Type Email address of new contact to add: ")
         contact_list[name] = email
-        new_cell0 = sheet.cell(x+1, 1)
-        new_cell1 = sheet.cell(x+1, 2)
-        new_cell2 = sheet.cell(x+1, 3)
-        new_cell0.value = x-1
+        new_cell0 = sheet.cell(x + 1, 1)
+        new_cell1 = sheet.cell(x + 1, 2)
+        new_cell2 = sheet.cell(x + 1, 3)
+        new_cell0.value = x - 1
         new_cell1.value = name
         new_cell2.value = email
         wb.save('contacts.xlsx')
@@ -115,5 +113,5 @@ def isnew_ornot():
 
 talk('hello! I am samantha your email bot')
 talk("you can exit at any point by saying 'quit'")
-isnew_ornot()
+new_or_not()
 wb.close()
